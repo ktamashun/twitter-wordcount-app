6ec90fef23f74e7884c95cc9c67376d5f9f687d8
ktamashun <kovacs.tamas.hun@gmail.com>
2017-02-13 23:11:05 +0100

    Added index.

--- database/dump.sql
+++ database/dump.sql
@@ -1,8 +1,9 @@
 --DROP TABLE IF EXISTS word_count;
 
 CREATE TABLE IF NOT EXISTS word_count (
- id INT NOT NULL AUTO_INCREMENT,
- word VARCHAR(100) NOT NULL,
- word_count INT(10) NOT NULL,
- PRIMARY KEY ( id )
-) engine=InnoDB;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(100) NOT NULL,
+  `word_count` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `word` (`word`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
